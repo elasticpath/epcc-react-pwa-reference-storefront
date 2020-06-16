a0d98f40d5db14a5ba9fea5881efb55752bfa54f
yulia-dnistrian <yulia.dnist@gmail.com>
2020-06-16 19:52:50 +0300

    MT-2815 add static components (#4)
    * update home page
    
    * create footer component & static components
    
    * add a link to the logo, remove comments
    
    * update home page

--- src/AppHeader.tsx
+++ src/AppHeader.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 import './AppHeader.scss';
+
+import headerLogo from './images/site-images/Company-Logo.svg';
 
 export const AppHeader: React.FC = (props) => {
   return (
     <div className="appheader">
       <div className="appheader__logo">
+        <Link to="/">
+          <img src={headerLogo} className="logo-image" alt=""/>
+        </Link>
       </div>
       <div className="appheader__moltincartcontainer">
         <span className="moltin-cart-button"></span>
