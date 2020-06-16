9cfa91825fff1cf9ca0912a82e9769f99cfdf0ac
Dusan Radovanovic <dusan.radovanovic@elasticpath.com>
2020-06-16 12:58:18 -0700

    Updated clientId

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 
 export const config = {
-  clientId: 'HvmUtpjmMSYmWs7zR367WYETN2aj51yXzxrxoWXu0x',
+  clientId: 'EdP3Gi1agyUF3yFS7Ngm8iyodLgbSR3wY4ceoJl0d2',
   stripeKey: 'pk_test_JwRX4cevuCysIEQrpwcbn3j8',
   categoryPageSize: 8,
 };
