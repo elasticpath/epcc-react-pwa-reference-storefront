d6d2f2be9810e9d2f5b75970b399998f0b1ac7ec
Dusan Radovanovic <dusan.radovanovic@elasticpath.com>
2020-06-20 19:51:43 -0700

    Addressed PR issues

--- src/Category.tsx
+++ src/Category.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import { loadCategoryProducts } from './service';
 import { useCategories } from './app-state';
 import { ProductThumbnail } from './ProductThumbnail';
