9b8510613bf6f4c8f5a47dfe6aaf2460066f3d46
Petro Dubno <petro.dubno@gmail.com>
2020-07-01 14:02:08 +0300

    Update mobile navigation menu

--- src/AppHeader.tsx
+++ src/AppHeader.tsx
@@ -3,29 +3,29 @@ import { Link } from "react-router-dom";
 import { ImageContainer } from './ImageContainer';
 import { useTranslation } from './app-state';
 import { LanguageDropdown } from './LanguageDropdown';
-
 import './AppHeader.scss';
 import headerLogo from './images/site-images/Company-Logo.svg';
 import { Navigation } from "./Navigation";
-
-
 export const AppHeader: React.FC = () => {
   const { t } = useTranslation();
-
   return (
     <div className="appheader">
-      <div className="appheader__logo">
-        <Link to="/" aria-label={t('bellvie-logo')}>
-          <ImageContainer imgUrl={headerLogo} imgClassName="logo-image" alt="logoImage"/>
-        </Link>
+      <div className="appheader__container">
+        <div className="appheader__logo">
+          <Link to="/" aria-label={t('bellvie-logo')}>
+            <ImageContainer imgUrl={headerLogo} imgClassName="logo-image" alt="logoImage"/>
+          </Link>
+        </div>
+        <div className="appheader__language">
+          <LanguageDropdown />
+        </div>
+        <div className="appheader__moltincartcontainer">
+          <span className="moltin-cart-button"></span>
+        </div>
       </div>
-      <div className="appheader__language">
-        <LanguageDropdown />
+      <div className="appheader__navigation">
+        <Navigation />
       </div>
-      <div className="appheader__moltincartcontainer">
-        <span className="moltin-cart-button"></span>
-      </div>
-      <Navigation />
     </div>
   );
 };
--- src/NavMenu.tsx
+++ src/NavMenu.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import { createCategoryUrl } from './routes';
 import { Category } from './service';
@@ -6,26 +6,40 @@ import { useCategories } from './app-state';
 
 import './NavMenu.scss';
 
-function renderCategories(categories: Category[], level: number = 0): React.ReactElement {
-  return (
-    <ul className={`navmenu__sub navmenu__sub--level-${level}`}>
-      {categories?.map(category => (
-        <li key={category.id} className={`navmenu__li navmenu__li--level-${level}`}>
-          <Link
-            className={`navmenu__link navmenu__link--level-${level}`}
-            to={createCategoryUrl(category.slug)}
-          >
-            {category.name}
-          </Link>
-          {category.children && renderCategories(category.children, level + 1)}
-        </li>
-      ))}
-    </ul>
-  );
-}
-
 export const NavMenu: React.FC = () => {
   const { categoriesTree } = useCategories();
+  const [isNavMenu, setHandleShow] = useState(false);
+
+  const handleShow = () => {
+    setHandleShow(!isNavMenu);
+  };
+
+  function renderCategories(categories: Category[], level: number = 0): React.ReactElement {
+
+    return (
+      <ul className={`navmenu__sub navmenu__sub--level-${level} dropdown-contents ${isNavMenu ? "show" : ""}`}>
+        {categories?.map(category => (
+          <li key={category.id} className={`navmenu__li navmenu__li--level-${level}`}>
+            {!category.children ? (
+              <Link
+                className={`navmenu__link navmenu__link--level-${level} ${category.children ? "has-children" : ""}`}
+                to={createCategoryUrl(category.slug)}
+              >
+                {category.name}
+              </Link>
+            ) : (
+              <button onClick={handleShow} className={`navmenu__link navmenu__link--level-${level} has-children`}>
+                {category.name}
+              </button>
+            )}
+            {category.children && renderCategories(category.children, level + 1)}
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
+
 
   return (
     <div className="navmenu">
