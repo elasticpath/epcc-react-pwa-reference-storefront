e4d43a7cf06b7f448b7fced5a6766ee2a27d22eb
Petro Dubno <petro.dubno@gmail.com>
2020-07-02 17:25:54 +0300

    Refreshing mobile navigation

--- src/NavMenu.tsx
+++ src/NavMenu.tsx
@@ -1,34 +1,51 @@
 import React, {useState} from 'react';
-import { Link } from 'react-router-dom';
+import {Link, useParams} from 'react-router-dom';
 import { createCategoryUrl } from './routes';
-import { Category } from './service';
+import {Category, Product} from './service';
 import { useCategories } from './app-state';
 
 import './NavMenu.scss';
 
-export const NavMenu: React.FC = () => {
+interface NavMenuProps {
+  isShowNavMenu: boolean;
+  isNavMenu: boolean;
+  categoryHistory: string[];
+  showNavigation: () => void;
+  handleCategoryClick: (id: string) => void;
+
+}
+
+export const NavMenu: React.FC<NavMenuProps> = (props) => {
   const { categoriesTree } = useCategories();
   const [isNavMenu, setHandleShow] = useState(false);
+  if (props.isNavMenu) {
+    setHandleShow(false);
+  }
+  const handleCloseMenu = () => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+    props.showNavigation();
+  };
 
-  const handleShow = () => {
+  const handleShow = (category: Category) => {
+    props.handleCategoryClick(category.id);
     setHandleShow(!isNavMenu);
   };
 
   function renderCategories(categories: Category[], level: number = 0): React.ReactElement {
-
     return (
-      <ul className={`navmenu__sub navmenu__sub--level-${level} dropdown-contents ${isNavMenu ? "show" : ""}`}>
+      <ul className={`navmenu__sub navmenu__sub--level-${level} dropdown-contents ${!props.isNavMenu ? "show" : "hide"}`}>
         {categories?.map(category => (
           <li key={category.id} className={`navmenu__li navmenu__li--level-${level}`}>
             {!category.children ? (
               <Link
+                onClick={handleCloseMenu}
                 className={`navmenu__link navmenu__link--level-${level} ${category.children ? "has-children" : ""}`}
                 to={createCategoryUrl(category.slug)}
               >
                 {category.name}
               </Link>
             ) : (
-              <button onClick={handleShow} className={`navmenu__link navmenu__link--level-${level} has-children`}>
+              <button onClick={() => handleShow(category)} className={`navmenu__link navmenu__link--level-${level} has-children`}>
                 {category.name}
               </button>
             )}
@@ -38,8 +55,6 @@ export const NavMenu: React.FC = () => {
       </ul>
     );
   }
-
-
 
   return (
     <div className="navmenu">
--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -1,19 +1,29 @@
 import React, { useState }  from 'react';
 import { Link } from 'react-router-dom';
 import useOnclickOutside from 'react-cool-onclickoutside';
+import { useTranslation } from './app-state';
 
 import { Category } from './service';
 import { useCategories } from './app-state';
+// import { handleShow } from './NavMenu';
 
 import './Navigation.scss';
 import { NavMenu } from './NavMenu';
 import { ReactComponent as MenuIcon } from './images/icons/ic_menu.svg';
+import { ReactComponent as CloseIcon } from './images/icons/ic_close.svg';
 
 let isDesktop = true;
 export const Navigation: React.FC = () => {
+  const { t } = useTranslation();
+  const { categoriesTree } = useCategories();
 
   const [isOpen, setIsOpen] = useState(false);
   const [isTopMenuOpen, setIsTopMenuOpen] = useState(false);
+  const [isNavMenu, setIsNavMenu] = useState(false);
+  const [categoryHistory, setCategoryHistory] = useState([]);
+
+
+
 
   function updatePredicate() {
     isDesktop = window.innerWidth  > 1092;
@@ -21,12 +31,21 @@ export const Navigation: React.FC = () => {
 
   window.addEventListener('resize', updatePredicate);
 
+  const handleCategoryClick = (categoryId: string) => {
+    // @ts-ignore
+    setCategoryHistory([...categoryHistory, categoryId])
+  };
+
   const showNavigation = () => {
     setIsTopMenuOpen(!isTopMenuOpen);
   };
 
   const handleSelectorClicked = () => {
     setIsOpen(!isOpen);
+  };
+
+  const handleBack = () => {
+    setIsNavMenu(true);
   };
 
   const reference = useOnclickOutside(() => {
@@ -45,17 +64,26 @@ export const Navigation: React.FC = () => {
 
     return (
       <div className="navigation">
+        <div className="navigation__header" ref={reference}>
+          {/* eslint-disable-next-line jsx-a11y/no-static-element-interactions,jsx-a11y/click-events-have-key-events */}
+          <div onClick={handleBack} className="navigation__header--left-arrow" />
+            <span>
+              {t('categories')}
+            </span>
+          <CloseIcon onClick={showNavigation} className="navigation__header--close" />
+        </div>
         <ul className={`navigation__sub navigation__sub--level-${level}`}>
           {topCategories?.map(category => (
             <li key={category.name} className={`navigation__li navigation__li--level-${level}`}>
               {category.url ? (
-                <Link
-                  className={`navigation__link navigation__link--level-${level}`}
-                  to={category.url}
-                  title={category.displayName}
-                >
-                  {category.displayName}
-                </Link>
+                  <Link
+                    className={`navigation__link navigation__link--level-${level}`}
+                    to={category.url}
+                    title={category.displayName}
+                    onClick={showNavigation}
+                  >
+                    {category.displayName}
+                  </Link>
               ) : (
                   <button className="navigation__link nav__item-link--has-children dropbtn" ref={reference} onClick={handleSelectorClicked}>{category.displayName}</button>
               )
@@ -64,13 +92,11 @@ export const Navigation: React.FC = () => {
           ))}
         </ul>
         <div ref={reference} className={`dropdown-content ${isOpen ? 'show' : ''}`}>
-          <NavMenu />
+          <NavMenu isShowNavMenu={isNavMenu} categoryHistory={categoryHistory} showNavigation={showNavigation} isNavMenu={isNavMenu} handleCategoryClick={handleCategoryClick} />
         </div>
       </div>
     );
   }
-
-  const { categoriesTree } = useCategories();
 
   return (
     <>
@@ -86,7 +112,6 @@ export const Navigation: React.FC = () => {
         <div className={`app-header-navigation-component ${isDesktop ? 'app-header-navigation-component--desktop-view' : 'app-header-navigation-component--mobile-view'}`}>
           {categoriesTree && renderCategories(categoriesTree)}
         </div>
-
       </nav>
     </>
   );
