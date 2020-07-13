bcb32fa2ef3e9ef78a9a33f73a90e7b3f582abc9
yulia-dnistrian <yulia.dnist@gmail.com>
2020-07-13 19:05:00 +0300

    update side menu and profile page

--- src/Profile.tsx
+++ src/Profile.tsx
@@ -1,12 +1,33 @@
 
 import React from 'react';
+import { useCustomerData, useTranslation } from './app-state';
 
-// import './Profile.scss'
+import './Profile.scss'
 
 export const Profile: React.FC = (props) => {
+  const { customerEmail, customerName } = useCustomerData();
+  const { t } = useTranslation();
+
   return (
     <div className="profile">
-      <h1>Profile</h1>
+      <h1 className="profile__title">{t('my-profile')}</h1>
+      <div className="profile__data">
+        <p className="profile__titlesmall">{t('general')}</p>
+        <div className="profile__container">
+          <h2>{t('personal-information')}</h2>
+          <div className="profile__info">
+            <p className="profile__infoitem">
+              <span className="profile__infolabel">{t('email')}:</span>
+              {customerEmail}
+            </p>
+            <p className="profile__infoitem">
+              <span className="profile__infolabel">{t('username')}:</span>
+              {customerName}
+            </p>
+            <button className="epbtn">{t('edit')}</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/SideMenu.tsx
+++ src/SideMenu.tsx
@@ -1,6 +1,7 @@
 
-import React from 'react';
+import React, {useState} from 'react';
 import { Link, useLocation } from 'react-router-dom';
+import useOnclickOutside from 'react-cool-onclickoutside';
 import { useTranslation } from "./app-state";
 import { createAccountUrl } from './routes';
 
@@ -10,6 +11,16 @@ export const SideMenu: React.FC = (props) => {
   const { t } = useTranslation();
   const accountUrl = createAccountUrl();
 
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleSelectorClicked = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const ref = useOnclickOutside(() => {
+    setIsOpen(false);
+  });
+
   const sideMenuItems = [
     { to: accountUrl, children: 'my-account' }
   ];
@@ -18,11 +29,11 @@ export const SideMenu: React.FC = (props) => {
 
   const location = useLocation();
   return (
-    <div className="sidemenu">
-      <button className="sidemenu__btn">
+    <div className="sidemenu" ref={ref}>
+      <button className="sidemenu__btn" onClick={handleSelectorClicked}>
         Test
       </button>
-      <div className="sidemenu__dropdown">
+      <div className={`sidemenu__dropdown ${!isOpen ? 'sidemenu__hidden' : ''}`}>
         {sideMenuItems.map(elem => (
           <div className='sidemenu__item'>
             <Link to={elem.to} className={`sidemenu__link ${location.pathname === elem.to ? '--selected' : ''}`}>{t(elem.children)}</Link>
