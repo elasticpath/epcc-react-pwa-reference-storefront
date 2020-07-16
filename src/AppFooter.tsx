9dd9e4da66a33cfc5dd8d8e91d8873f18f13d43a
yulia-dnistrian <yulia.dnist@gmail.com>
2020-07-16 20:58:46 +0300

    update loader on category and product page (#39)

--- src/AppFooter.tsx
+++ src/AppFooter.tsx
@@ -22,69 +22,71 @@ export const AppFooter: React.FC = (props) => {
   };
 
   return (
-    <div className="container">
-      <div className="app-footer">
-        <div className="first-row">
-          <div className="footer-column">
-            <div className="title">
-              {t('bellvie')}
+    <div className="appfooter">
+      <div className="container">
+        <div className="appfooter__content">
+          <div className="appfooter__firstrow">
+            <div className="appfooter__column">
+              <div className="appfooter__title">
+                {t('bellvie')}
+              </div>
+              <div className="appfooter__txt">
+                {t('footer-1-content')}
+              </div>
             </div>
-            <div className="content">
-              {t('footer-1-content')}
+            <div className="appfooter__column">
+              <div className="appfooter__title">
+                {t('assistance')}
+              </div>
+              <div className="appfooter__txt">
+                <Link to={linkTo.aboutus}>
+                  {t('about-us')}
+                </Link>
+                <Link to={linkTo.contactus}>
+                  {t('contact')}
+                </Link>
+                <Link to={linkTo.shippingreturns}>
+                  {t('shipping-and-returns')}
+                </Link>
+                <Link to={linkTo.termsandconditions}>
+                  {t('terms-and-conditions')}
+                </Link>
+              </div>
+            </div>
+            <div className="appfooter__column appfooter__social">
+              <div className="appfooter__title">
+                {t('find-us-online')}
+              </div>
+              <div className="appfooter__txt">
+                <Link to={linkTo.shareFacebook} aria-label={t('share-facebook')}>
+                  <FacebookIcon className="appfooter__icon" />
+                  {t('facebook')}
+                </Link>
+                <Link to={linkTo.shareTwitter} aria-label={t('share-twitter')}>
+                  <TwitterIcon className="appfooter__icon" />
+                  {t('twitter')}
+                </Link>
+                <Link to={linkTo.shareInstagram} aria-label={t('share-instagram')}>
+                  <InstagramIcon className="appfooter__icon" />
+                  {t('instagram')}
+                </Link>
+              </div>
             </div>
           </div>
-          <div className="footer-column">
-            <div className="title">
-              {t('assistance')}
-            </div>
-            <div className="content">
-              <Link to={linkTo.aboutus}>
-                {t('about-us')}
-              </Link>
-              <Link to={linkTo.contactus}>
-                {t('contact')}
-              </Link>
-              <Link to={linkTo.shippingreturns}>
-                {t('shipping-and-returns')}
-              </Link>
-              <Link to={linkTo.termsandconditions}>
-                {t('terms-and-conditions')}
-              </Link>
-            </div>
-          </div>
-          <div className="footer-column social">
-            <div className="title">
+          <div className="appfooter__secondrow">
+            <div className="appfooter__title">
               {t('find-us-online')}
             </div>
-            <div className="content">
-              <Link to={linkTo.shareFacebook} aria-label={t('share-facebook')}>
-                <FacebookIcon className="share-icon" />
-                {t('facebook')}
-              </Link>
-              <Link to={linkTo.shareTwitter} aria-label={t('share-twitter')}>
-                <TwitterIcon className="share-icon" />
-                {t('twitter')}
-              </Link>
-              <Link to={linkTo.shareInstagram} aria-label={t('share-instagram')}>
-                <InstagramIcon className="share-icon" />
-                {t('instagram')}
-              </Link>
-            </div>
+            <Link to={linkTo.shareFacebook} aria-label={t('share-facebook')}>
+              <FacebookIcon className="appfooter__icon" />
+            </Link>
+            <Link to={linkTo.shareTwitter} aria-label={t('share-twitter')}>
+              <TwitterIcon className="appfooter__icon" />
+            </Link>
+            <Link to={linkTo.shareInstagram} aria-label={t('share-instagram')}>
+              <InstagramIcon className="appfooter__icon" />
+            </Link>
           </div>
-        </div>
-        <div className="second-row">
-          <div className="title">
-            {t('find-us-online')}
-          </div>
-          <Link to={linkTo.shareFacebook} aria-label={t('share-facebook')}>
-            <FacebookIcon className="share-icon" />
-          </Link>
-          <Link to={linkTo.shareTwitter} aria-label={t('share-twitter')}>
-            <TwitterIcon className="share-icon" />
-          </Link>
-          <Link to={linkTo.shareInstagram} aria-label={t('share-instagram')}>
-            <InstagramIcon className="share-icon" />
-          </Link>
         </div>
       </div>
     </div>
