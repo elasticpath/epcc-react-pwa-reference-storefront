59de3320d4f9184124dc243c04482790d73b6274
Alvin <alvin.chan@elasticpath.com>
2020-07-17 11:28:58 -0400

    Changes to the LoginDialog

--- src/LoginDialog/LoginDialog.tsx
+++ src/LoginDialog/LoginDialog.tsx
@@ -43,8 +43,6 @@ export const LoginDialog: React.FC<AppModalLoginMainProps> = (props) => {
   console.log('printing out the authentication profiles');
   console.log(authenticationProfiles);
   
-  const location = useLocation();
-  
   const { t } = useTranslation();
 
   const [isLoading, setIsLoading] = useState(false);
@@ -55,41 +53,8 @@ export const LoginDialog: React.FC<AppModalLoginMainProps> = (props) => {
     handleModalClose();
   }
 
-  // TODO: We need to remember what page we were on before we left as well...
+  // TODO: We need to remember what page we were on before we left as well..
 
-  let keycloakLoginRedirectUrl = '';
-  
-  // HARDCODE THIS FOR NOW...  
-  const generateStateToken = () => {
-    // This is going to generate the state token
-    return Math.random().toString(36).substr(2) + Math.random().toString(36).substr(2);
-  }
-
-  
-  const generateKeycloakLoginRedirectUrl = () => {
-    // TODO: This function is going to generate the keycloak redirection url...
-    // keycloakLoginRedirectUrl = `http://localhost:24074/auth/realms/Sample/protocol/openid-connect/auth?client_id=epcc-reference-store&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fkeycloak&state=12g45e18-98f0-4c26-a96c-b3d9a0621a4e&response_mode=fragment&response_type=code&scope=openid&nonce=e2071a63-d81b-4cfe-87ee-991bda771bf0`;
-    
-    const baseRedirectUrl = `http://localhost:24074/auth/realms/Sample/protocol/openid-connect/auth?`
-    // const client_id = authenticationSettings?.data.meta.clientId
-    const clientId = `client_id=${authenticationSettings?.data.meta.clientId}` // Should be able to get this from the authentication-settings.
-    const redirectUri = `redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fkeycloak` // TODO... We need to keep it logged in with the current uri
-    const stateToken = generateStateToken();
-    const state = `state=${stateToken}`
-    localStorage.setItem('state', stateToken);
-    
-    console.log('this is the token im setting into localStorage');
-    console.log(stateToken);
-    
-    localStorage.setItem('location', location.pathname); // Save the location on callback
-    
-    const responseMode = 'response_mode=fragment'
-    const responseType = 'response_type=code'
-    const scope = 'scope=openid'
-    
-    keycloakLoginRedirectUrl = `${baseRedirectUrl}${clientId}&${redirectUri}&${state}&${responseMode}&${responseType}&${scope}`
-    return keycloakLoginRedirectUrl;
-  }
   return (
     <Modal open={openModal} onClose={handleClose} classNames={{ modal: 'logindialog' }} showCloseIcon={false}>
       {
@@ -115,11 +80,11 @@ export const LoginDialog: React.FC<AppModalLoginMainProps> = (props) => {
           <LoginDialogDivider/>]
         } */}
 
-        { authenticationSettings ?
-          (authenticationSettings?.data.allow_password_authentication &&
+        { 
+          authenticationSettings?.data.allow_password_authentication ?
           <PasswordLoginForm handleModalClose={handleModalClose} isLoading={isLoading} setIsLoading={setIsLoading} setFailedLogin={setFailedLogin} />
           /** We should place the OIDC buttons here... */
-          ) :
+           :
           <div className="epminiLoader --centered" />
         }
         
