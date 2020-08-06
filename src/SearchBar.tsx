ad42d2df6f522c3b2f81ee125f28cfa80a636243
Barton Dong <barton97daz@gmail.com>
2020-08-06 13:21:02 -0400

    [[MT-3568]add voicesearch functionality and adjust styles

--- src/SearchBar.tsx
+++ src/SearchBar.tsx
@@ -1,17 +1,74 @@
 import React, { useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import useOnclickOutside from 'react-cool-onclickoutside';
-import { SearchBox, Hits, PoweredBy } from 'react-instantsearch-dom';
+import { SearchBox, Hits, PoweredBy, VoiceSearch } from 'react-instantsearch-dom';
 import { useTranslation } from './app-state';
 import { createSearchUrl } from './routes';
 
 import { ReactComponent as MagnifyingGlassIcon } from './images/icons/magnifying_glass.svg';
 import { ReactComponent as ClearIcon } from './images/icons/ic_clear.svg';
+import Icon_SearchMic from './images/icons/icon-search-mic.svg'
+import Icon_SearchMicHover from './images/icons/icon-search-mic-hover.svg'
+import Icon_SearchMicActive from './images/icons/icon-search-mic-active.svg'
 
 import './SearchBar.scss';
+import styled, { keyframes } from 'styled-components'
 
 interface SearchBoxProps {
 }
+
+interface VoiceSearchProps{
+  defaultIcon: any;
+  hoverIcon: any;
+  activeIcon: any;
+}
+
+const SearchButton = styled.button<VoiceSearchProps>`
+  all: unset;
+  display: block;
+  width: 36px;
+  height: 36px;
+  border-radius: 36px;
+  margin-left: 8px;
+  cursor: pointer;
+
+  background-image: url(${props => props.defaultIcon});
+  background-repeat: no-repeat;
+  background-position: center;
+
+  &:hover {
+    background-image: url(${props => props.hoverIcon});
+  }
+
+  &:active {
+    background-image: url(${props => props.activeIcon});
+  }
+`
+
+const breatheAnimation = keyframes`
+  100% { opacity: 1 }
+  50% { opacity: 0.65 }
+`
+
+const SearchButtonBreating = styled(SearchButton)`
+  animation-name: ${breatheAnimation};
+  animation-duration: 0.9s;
+  animation-iteration-count: infinite;
+`
+
+const SearchButtonMic = (props: any) => (
+  props.isListening ?
+    <SearchButtonBreating as="span"
+      defaultIcon={Icon_SearchMic}
+      hoverIcon={Icon_SearchMicHover}
+      activeIcon={Icon_SearchMicActive}
+      {...props} /> :
+    <SearchButton as="span"
+      defaultIcon={Icon_SearchMic}
+      hoverIcon={Icon_SearchMicHover}
+      activeIcon={Icon_SearchMicActive}
+      {...props} />
+)
 
 export const SearchBar: React.FC<SearchBoxProps> = () => {
   const { t } = useTranslation();
@@ -52,6 +109,25 @@ export const SearchBar: React.FC<SearchBoxProps> = () => {
     setInputVisible(false);
   };
 
+  const VoiceSearchButtonText = ({
+    isListening,
+    isBrowserSupported
+  }: {isListening:any, isBrowserSupported:any}) => (
+    isBrowserSupported ? (
+      isListening ?
+        <SearchButtonMic
+          defaultIcon  ={Icon_SearchMicActive}
+          isListening  = {isListening}
+        /> :
+        <SearchButtonMic
+          isListening  = {isListening}
+          onClick={() => {
+            handleFocus()
+          }}
+        />
+    ) : null
+  )
+
   const Hit = ({ hit }: any) => {
     return (
       <Link className="searchbar__hint" to={`/product/${hit.slug}`} onClick={handleLinkClick}>
@@ -87,6 +163,10 @@ export const SearchBar: React.FC<SearchBoxProps> = () => {
           submit={<MagnifyingGlassIcon />}
           reset={<ClearIcon />}
           translations={translations}
+        />
+        <VoiceSearch
+          searchAsYouSpeak
+          buttonTextComponent = {VoiceSearchButtonText}
         />
         <button
           className={`searchbar__close ${searchValue && '--show'}`}
