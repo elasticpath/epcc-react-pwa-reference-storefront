52c455dd5d8c1e7d7bcfc9296d7be7e9d9e68053
yasiloghmani <yasaman.loghmani@elasticpath.com>
2021-03-22 14:41:01 -0400

    create components

---
+++ src/CartsDetailsPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import useOnclickOutside from 'react-cool-onclickoutside';
+
+import './CartsList.scss';
+
+export  const CartsDetailsPage: React.FC = () => {
+  return (
+      <div className='cartsdetailspage'>
+          
+      </div>
+  )
+};
---
+++ src/MyCartsList.tsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+
+import './MyCartsList.scss';
+
+export  const MyCartsList: React.FC = () => {
+  return (
+      <div className='mycarts'>
+        <div className="container">
+            <div  className='mycarts__header'>
+                <h1 className="mycarts__title">My Carts</h1>
+            </div>
+           
+        </div>
+      </div>
+  )
+};
